7fa2f5327e21933b50b138138f3e2b4086d61349
e23oneil35 <emo82@cornell.edu>
2025-09-30 12:50:11 -0400

    commit to github

---
+++ class_notes/importing data sets.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(readxl)##install.packages("readxl")
+library(googlesheets4)  ##install.packages("googlesheets4")
+library(janitor)##install.packages("janitor")
+## grabbing something online
+lotr <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/lotr_tidy.csv")
+
+write_csv(lotr, file = "./data/lotr.csv")
+
+lotr<- read.csv("data/lotr.csv")
+
+lotr<- read.csv("Users/emily/OneDrive/Desktop/Git Hub/ntres-6100-assignments-e23oneil35/data/lotr.csv")
+
+lotr<- read.csv("data/lotr.csv", skip = 1, comment = "#")
+
+lotr <- read_xlsx("data/data_lesson11.xlsx", sheet = "FOTR")
+
+
+gs4_deauth()
+lotr_google <- read_sheet("https://docs.google.com/spreadsheets/d/1X98JobRtA3JGBFacs_JSjiX-4DPQ0vZYtNl_ozqF6IE/edit#gid=754443596", sheet = "FOTR", range = "A5:F15")
+
+
+msa <- read_tsv("https://raw.githubusercontent.com/nt246/NTRES-6100-data-science/main/datasets/janitor_mymsa_subset.txt")
+view(msa)
+
+colnames(msa)
+msa_clean <- clean_names(msa, case = "upper_camel")
+
+cbind(colnames(msa), colnames(msa_clean))
+
+
+parse_number("$100")
+parse_number("80%")
+parse_number("It cost $100")
